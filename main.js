d91be51e6a2b5132dea74f9776f485de06c5f5b7
mohamed jalloh <m.ouryjalloh@yahoo.com>
2018-11-30 23:22:44 -0500

    last js file added

---
+++ main.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("btn");
+// let div = document.querySelector(".seconddiv")[0];
+
+btn.addEventListener("click",onclick);
+
+function onclick(){
+   document.getElementById("seconddiv").classList.toggle("try");
+}
+
+
+
+
